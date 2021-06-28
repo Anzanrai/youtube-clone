49642bb25e9953edf75d71935b0cf869d5864234
Anjan Rai <anjanraiz@Anjans-MacBook-Pro.local>
2021-06-28 21:51:29 +1000

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyD_X2NE1AJgEum3mv3hf9-sjH94VxQJ0Vg',
+  authDomain: 'fir-b6286.firebaseapp.com',
+  projectId: 'fir-b6286',
+  storageBucket: 'fir-b6286.appspot.com',
+  messagingSenderId: '392016671351',
+  appId: '1:392016671351:web:13a6b0e809ac78c036cc72',
+  measurementId: 'G-6LXWQKZKSQ',
+};
